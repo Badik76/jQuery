208f48f3f230fc9f0e71856141ccd7fd98dafb8a
Karl <badik76@gmail.Comgit>
2018-10-12 09:45:57 +0200

    fin exo5

--- Partie2/exo5/assets/script.js
+++ Partie2/exo5/assets/script.js
@@ -1,1 +1,12 @@
-$('#text').hide();
+$("#lastName").focus(function(){
+  $("#lastName").css("border","1px solid green")
+})
+$("#firstName").focus(function(){
+  $("#firstName").css("border","1px solid green")
+})
+$("#lastName").focusout(function(){
+  $("#lastName").css("border","1px solid red")
+})
+$("#firstName").focusout(function(){
+  $("#firstName").css("border","1px solid red")
+})
