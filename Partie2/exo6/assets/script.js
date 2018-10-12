03afcfc0c7d96db8afdb963849b4a0afffe36ad7
Karl <badik76@gmail.Comgit>
2018-10-12 09:55:11 +0200

    fin exo6, les carrés de couleurs <3

--- Partie2/exo6/assets/script.js
+++ Partie2/exo6/assets/script.js
@@ -1,1 +1,12 @@
-$('#text').hide();
+$("#green").mouseover(function(){
+$("#text").css("color","green")
+})
+$("#red").mouseover(function(){
+$("#text").css("color","red")
+})
+$("#blue").mouseover(function(){
+$("#text").css("color","blue")
+})
+$("#blue, #red, #green").mouseout(function(){
+$("#text").css("color","white")
+})
