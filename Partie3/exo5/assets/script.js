a2d404ae8a91dbca9644c35803c0797847f3a774
Karl <badik76@gmail.Comgit>
2018-10-12 16:30:49 +0200

    carré qui bouge sans délimitation

--- Partie3/exo5/assets/script.js
+++ Partie3/exo5/assets/script.js
@@ -1,12 +1,26 @@
-$("#lastName").focus(function(){
-  $("#lastName").css("border","1px solid green")
-})
-$("#firstName").focus(function(){
-  $("#firstName").css("border","1px solid green")
-})
-$("#lastName").focusout(function(){
-  $("#lastName").css("border","1px solid red")
-})
-$("#firstName").focusout(function(){
-  $("#firstName").css("border","1px solid red")
-})
+$('#text').keydown(function(e){
+  // var WW = $(window).width() //définition d'un variable sur la hauteur max
+  // var WH = $(window).height()//définition d'une variable sur la longueur max
+  // var wh = WH < $("#yellow").height() //variable
+  // var ww = WW < $("#yellow").width() //  variable
+
+  if (e.which == 39) { //Vers la droite
+    posX = parseInt($('#yellow').css('left'));
+      $('#yellow').css('left', posX+10);
+  }
+  if (e.which == 37) { // Vers la gauche
+    posX = parseInt($('#yellow').css('left'));
+      $('#yellow').css('left', posX-10);
+  }
+  if (e.which == 40) {// Vers le bas
+    posY = parseInt($('#yellow').css('top'));
+      $('#yellow').css('top', posY+10);
+  }
+  if (e.which == 38) {// Vers le haut
+    posY = parseInt($('#yellow').css('top'));
+      $('#yellow').css('top', posY-10);
+  }
+
+  var offset = $('#yellow').offset();
+  offset.left
+});
