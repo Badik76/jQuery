3c42f0b9e32203e77a5650451bc82b4377dab4b4
Karl <badik76@gmail.Comgit>
2018-10-12 11:00:40 +0200

    fin exo3, bouton +;-

--- Partie3/exo2/assets/script.js
+++ Partie3/exo2/assets/script.js
@@ -1,3 +1,13 @@
-$("#image").dblclick(function(){
-  $("#image").css({"width":"500px"})
-})
+$(document).ready(function(){
+        //var counter = $('#TextBox').val();
+        $('#clickMe').click( function() {
+            var counter = $('#textbox').val();
+            counter++ ;
+            $('#textbox').val(counter);
+    });
+    $('#clickme').click( function() {
+        var counter = $('#textbox').val();
+        counter-- ;
+        $('#textbox').val(counter);
+});
+});
