1521c47c381c5d5890e9caef2a39b9b27b32ced9
Karl <badik76@gmail.Comgit>
2018-10-16 12:25:44 +0200

    fin exo 4 formulaire mais regex à revoir

--- Partie4/exo3/assets/script.js
+++ Partie4/exo3/assets/script.js
@@ -1,15 +1,16 @@
-var choixOrdi = Math.floor((Math.random() * 100) + 1); //vaiable qui créer un chiffre aléatoire à trouver.
-var compteur = 0 //la variable qui compte
-$("#submit").on("click", function(){ //définition d'une fonction click au bouton
-var choixUtilisateur = parseInt($("#textbox").val()); //variable du choix de l'utilisateur.
-  // console.log(choixOrdi); //choix de l'ordi dans la console. En com car nous ne voulons pas l'affichier
-    compteur++     //le compteur qui monte à chaque click
-  if (choixOrdi !== choixUtilisateur) { //fonction if si le résultat n'est pas égal
-    if (choixUtilisateur > choixOrdi) { //if si choix utilisateur est supérieur à celui de l'ordi
-      alert("Pluuus Ptit mon p'tit !");//réponse affichée
-    }else{//si utilisateur < ordi alors
-  alert("PLUUUUUUUS HAUUUUUT, plus graaaand, pluuuuus fort !"); // réponse si uti<ordinateur
-}}else{//si ordi=uti alors
-  alert("Correcte, Félicitation t'es un bon, tu as trouvé le bon chiffre bravo après " + compteur +" tentatives.");//si réponse positive du premier if
-}//fin du else
-})//fin de la fonction click.
+(function() {
+  window.addEventListener('load', function() {
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.getElementsByClassName('needs-validation');
+  // Loop over them and prevent submission
+  var validation = Array.prototype.filter.call(forms, function(form) {
+    form.addEventListener('submit', function(event) {
+      if (form.checkValidity() === false) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+      form.classList.add('was-validated');
+    }, false);
+  });
+}, false);
+})
